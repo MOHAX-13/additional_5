303a7c3d4d3ee0c458641ed0853e3c5fe4994261
DENIS <MOHAX1983@gmail.com>
2018-09-28 23:39:36 +0300

    6additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+	for (var d = /(\[|\(|\{)[^\[\(\{]*?$/,
+		e = {
+			"(": /\([^\}\]]*?\)/,
+			"[": /\[[^\)\}]*?\]/,
+			"{": /\{[^\)\]]*?\}/,
+		}, b, c = !0; c;)
+		b = str, str = str.replace(d, function (str, b)
+		{
+			return str.replace(e[b], "")
+		}), b == str && (c = !1);
+	return !/[\[\]\(\)\{\}]/.test(str)
 }
